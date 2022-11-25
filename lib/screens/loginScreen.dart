f16da3aaee389d2800efe0af7e29bd3cffe388e3
NiGELLA <info@nigellasoftwares.com>
2022-11-25 11:42:37 +0530

    first commit

---
+++ lib/screens/loginScreen.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class loginScreen extends StatefulWidget{
+  @override
+  State<loginScreen> createState()=> loginScreenState();
+}
+
+class loginScreenState extends State<loginScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw Scaffold(
+
+    );
+  }
+
+
+}
